b9c873d666690cb1ac32fca790c2da0dd98ebd30
pcanning101 <60663746+pcanning101@users.noreply.github.com>
2020-02-04 19:23:20 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,7 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'THis is a readme',
-    owner   => 'root',
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
 
   
